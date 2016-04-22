6049a93d437044de98f6a30c80738a326a3927f3
rkoeninger <rkoeninger@att.net>
2016-04-22 16:00:06 -0400

    Improved presentation of env and stack
    Added swap3, nilp, -, * and pos, clear verbs
    Preserved stack between inputs

--- Main.elm
+++ Main.elm
@@ -1,4 +1,4 @@
-import Html exposing (Html, Attribute, text, toElement, div, input)
+import Html exposing (Html, Attribute, text, toElement, div, input, table, tr, td, ul, li)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, targetValue, keyCode)
 import Signal exposing (Address)
@@ -9,42 +9,43 @@ import Dict exposing (Dict)
 import Json.Decode as JD
 import Result
 
-main = StartApp.start { model = ("", "", initEnv), view = view, update = update }
+main = StartApp.start { model = ("", [], initEnv), view = view, update = update }
 
 update newState oldState = newState
 
-view address (expr, output, env) =
+view address (expr, stack, env) =
     let parsedExpr =
         String.words expr
         |> List.filter (\s -> not (String.isEmpty s))
         |> List.map parse in
     let reStringedExpr =
         parsedExpr
-        |> List.map toString
+        |> List.map showValue
         |> String.join " | " in
     div []
         [ input
             [ placeholder "Enter a Jenga Expression"
             , value expr
-            , on "input" (JD.map (\v -> (v, output, env)) targetValue) (Signal.message address)
-            , onEnter address env parsedExpr
-            , myStyle
+            , on "input" (JD.map (\v -> (v, stack, env)) targetValue) (Signal.message address)
+            , onEnter address env stack parsedExpr
+            , inputStyle
             ]
             []
-        , div [ myStyle ] [ text reStringedExpr ]
-        , div [ myStyle ] [ text output ]
+        , div [ inputStyle ] [ text reStringedExpr ]
+        , div [ statusStyle ] [ showEnv env ]
+        , div [ statusStyle ] [ showStack stack ]
         ]
 
-onEnter address env parsedExpr =
+onEnter address env stack parsedExpr =
     on "keydown"
         (JD.customDecoder keyCode is13)
         (\_ ->
-            let (newEnv, newStack) = eval env [] parsedExpr in
-                Signal.message address ("", toString newEnv ++ toString newStack, newEnv))
+            let (newEnv, newStack) = eval env stack parsedExpr in
+                Signal.message address ("", newStack, newEnv))
 
 is13 code = if code == 13 then Ok () else Err "Not the right code"
 
-myStyle =
+inputStyle =
     style
         [ ("width", "100%")
         , ("height", "40px")
@@ -52,6 +53,35 @@ myStyle =
         , ("font-size", "2em")
         , ("text-align", "center")
         ]
+
+statusStyle =
+    style
+        [ ("width", "45%")
+        , ("padding", "10px")
+        , ("font-size", "2em")
+        , ("display", "inline-block")
+        ]
+
+showVerb verb =
+    case verb of
+        Primitive _ -> "<Primitive>"
+        Defined line -> line |> List.map showValue |> String.join " "
+
+showEnv env =
+    let envItem (name, verb) = tr [] [td [] [text name], td [] [text (showVerb verb)]] in
+    table [] (Dict.toList env |> List.map envItem)
+
+showStack stack =
+    let stackItem value = li [] [text (showValue value)] in
+    ul [] (List.map stackItem stack)
+
+showValue value =
+    case value of
+        JNil -> "nil"
+        JBool x -> if x then "true" else "false"
+        JInt x -> toString x
+        JSym name -> name
+        JCons (x, y) -> "(" ++ showValue x ++ " " ++ showValue y ++ ")"
 
 type JValue = JNil
             | JBool Bool
@@ -71,16 +101,16 @@ type alias Env = Dict String Verb
 eval : Env -> Stack -> Line -> (Env, Stack)
 eval env stack line =
     case line of
-        JSym ":" :: JSym x :: xs -> (define env x xs, stack)
+        JSym ":" :: JSym name :: restLine -> (define env name restLine, stack)
         JSym "if" :: restLine ->
             case stack of
                 JBool True :: restStack -> eval env restStack (ifTrue restLine)
                 JBool False :: restStack -> eval env restStack (ifFalse restLine)
                 _ -> Debug.crash "If expects Bool on top of stack"
-        JNil :: xs -> eval env (JNil :: stack) xs
-        JInt x :: xs -> eval env (JInt x :: stack) xs
-        JBool x :: xs -> eval env (JBool x :: stack) xs
-        JSym x :: xs -> eval env (apply env stack (lookup env x)) xs
+        JNil :: restLine -> eval env (JNil :: stack) restLine
+        JInt x :: restLine -> eval env (JInt x :: stack) restLine
+        JBool x :: restLine -> eval env (JBool x :: stack) restLine
+        JSym name :: restLine -> eval env (apply env stack (lookup env name)) restLine
         [] -> (env, stack)
         _ -> Debug.crash "??? Not implemented"
 
@@ -88,9 +118,7 @@ apply : Env -> Stack -> Verb -> Stack
 apply env stack verb =
     case verb of
         Primitive f -> f stack
-        Defined line ->
-            case eval env stack line of
-                (_, newStack) -> newStack
+        Defined line -> eval env stack line |> snd
 
 define : Env -> String -> Line -> Env
 define env name line = Dict.insert name (Defined line) env
@@ -105,34 +133,34 @@ parse s =
                                   |> Maybe.withDefault (JSym s)
 
 lookup : Env -> String -> Verb
-lookup env sym =
-    case Dict.get sym env of
+lookup env name =
+    case Dict.get name env of
         Just f -> f
         Nothing -> Debug.crash "Symbol not defined"
 
 initEnv : Env
 initEnv =
-    [ ("dup",   dup)
+    [ ("clear", clear)
+    , ("dup",   dup)
     , ("drop",  drop)
     , ("swap",  swap)
+    , ("swap3", swap3)
     , ("cons",  cons)
     , ("consp", consp)
     , ("hd",    hd)
     , ("tl",    tl)
+    , ("nilp",  nilp)
     , ("+",     add)
+    , ("-",     sub)
+    , ("*",     mul)
+    , ("pos",   pos)
     ]
-    |> List.map (\(sym, f) -> (sym, Primitive f))
+    |> List.map (\(name, f) -> (name, Primitive f))
     |> Dict.fromList
 
-isElseSym x =
-    case x of
-        JSym "else" -> True
-        _ -> False
+isElseSym = (==) (JSym "else")
 
-isThenSym x =
-    case x of
-        JSym "then" -> True
-        _ -> False
+isThenSym = (==) (JSym "then")
 
 takeUntil f list =
     case list of
@@ -150,43 +178,73 @@ ifTrue line = takeUntil isElseSym line ++ takeAfter isThenSym line
 -- TODO: this won't (?) work with nested if's
 ifFalse line = takeAfter isElseSym (takeUntil isThenSym line) ++ takeAfter isThenSym line
 
+clear stack =
+    case stack of
+        _ -> []
+
 dup stack =
     case stack of
-        x :: xs -> x :: x :: xs
+        x :: restStack -> x :: x :: restStack
         _ -> Debug.crash "Stack must have at least 1 item"
 
 drop stack =
     case stack of
-        _ :: xs -> xs
+        _ :: restStack -> restStack
         _ -> Debug.crash "Stack must have at least 1 item"
 
 swap stack =
     case stack of
-        x :: y :: xs -> y :: x :: xs
+        x :: y :: restStack -> y :: x :: restStack
         _ -> Debug.crash "Stack must have at least 2 items"
+
+swap3 stack =
+    case stack of
+        x :: y :: z :: restStack -> z :: y :: x :: restStack
+        _ -> Debug.crash "Stack must have at least 3 items"
 
 cons stack =
     case stack of
-        x :: y :: xs -> JCons (x, y) :: xs
+        x :: y :: restStack -> JCons (x, y) :: restStack
         _ -> Debug.crash "Stack must have at least 2 items"
 
 consp stack =
     case stack of
-        JCons _ :: xs -> JBool True :: xs
-        _ :: xs -> JBool False :: xs
+        JCons _ :: restStack -> JBool True :: restStack
+        _ :: restStack -> JBool False :: restStack
         _ -> Debug.crash "Stack must have at least 1 item"
 
 hd stack =
     case stack of
-        JCons (x, _) :: xs -> x :: xs
+        JCons (x, _) :: restStack -> x :: restStack
         _ -> Debug.crash "Stack must have a Cons on top"
 
 tl stack =
     case stack of
-        JCons (_, x) :: xs -> x :: xs
+        JCons (_, x) :: restStack -> x :: restStack
         _ -> Debug.crash "Stack must have a Cons on top"
+
+nilp stack =
+    case stack of
+        JNil :: restStack -> JBool True :: restStack
+        _ :: restStack -> JBool False :: restStack
+        _ -> Debug.crash "Stack must have at least 1 item"
 
 add stack =
     case stack of
-        JInt x :: JInt y :: xs -> JInt (x + y) :: xs
+        JInt x :: JInt y :: restStack -> JInt (y + x) :: restStack
         _ -> Debug.crash "Stack must have 2 numbers on top"
+
+sub stack =
+    case stack of
+        JInt x :: JInt y :: restStack -> JInt (y - x) :: restStack
+        _ -> Debug.crash "Stack must have 2 numbers on top"
+
+mul stack =
+    case stack of
+        JInt x :: JInt y :: restStack -> JInt (y * x) :: restStack
+        _ -> Debug.crash "Stack must have 2 numbers on top"
+
+pos stack =
+    case stack of
+        JInt x :: restStack -> JBool (x > 0) :: restStack
+        _ -> Debug.crash "Stack must have number on top"
